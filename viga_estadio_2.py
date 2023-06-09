66c4a90350fd24810b89824998ee38b813fe23b4
PM1980 <39511663+PM1980@users.noreply.github.com>
2023-06-09 15:57:27 -0300

    Update viga_estadio_2.py

--- viga_estadio_2.py
+++ viga_estadio_2.py
@@ -1,44 +1,45 @@
 import streamlit as st
-import sympy as sp
 
-def calculate_moment_of_resistance(b, d, Aaco, Eaco, Econc, sigma_aco, sigma_conc):
-    n = Eaco / Econc
-    x = sp.symbols('x')
-    eq_linha_neutra = b * x**2/2 - n * Aaco * (d-x)
-    sol = sp.solve(eq_linha_neutra)
-    x = max(sol)
-    Ieq = (b*x**3/12) + (b*x)*(x/2)**2 + n * Aaco * (d-x)**2
+def calculate_moment_of_resistance(b, d, Aaço, Eaço, Econc, sigma_aço, sigma_conc):
+    n = Eaço / Econc
+    x = (-n*Aaço + ((n*Aaço)**2 +2*b*n*Aaço*d)**(1/2)) * (1/b)
+    Ieq = (b*x**3/12) + (b*x)*(x/2)**2 + n * Aaço * (d-x)**2
     Mconc = sigma_conc * Ieq / x
-    Maco = sigma_aco * Ieq / ((d-x) * n)
-    return min(Mconc, Maco)
+    Maço = sigma_aço * Ieq / ((d-x) * n)
+    Madm = min(Mconc, Maço)
+    return x, Ieq, Mconc, Maço, Madm
 
 def main():
     st.title("Moment of Resistance Calculator")
     st.write("Enter the values to calculate the moment of resistance")
 
-    b = st.text_input("Base of the section (m)")
-    d = st.text_input("Effective height of the section (m)")
-    Aaco = st.text_input("Total area of reinforcement (m2)")
-    Eaco = st.text_input("Elastic modulus of steel (GPa)")
-    Econc = st.text_input("Elastic modulus of concrete (GPa)")
-    sigma_aco = st.text_input("Allowable stress for steel (Pa)")
-    sigma_conc = st.text_input("Allowable stress for concrete (Pa)")
+    b = st.text_input("Base of the section (m)", value='0.20')
+    d = st.text_input("Effective height of the section (m)", value='0.50')
+    Aaço = st.text_input("Total area of reinforcement (m2)")
+    Eaço = st.text_input("Elastic modulus of steel (GPa)", value='200')
+    Econc = st.text_input("Elastic modulus of concrete (GPa)", value='20')
+    sigma_aço = st.text_input("Allowable stress for steel (Pa)", value='250e6')
+    sigma_conc = st.text_input("Allowable stress for concrete (Pa)", value='10e6')
 
     if st.button("Calculate"):
         try:
             b = float(b)
             d = float(d)
-            Aaco = float(Aaco)
-            Eaco = float(Eaco)
+            Aaço = eval(Aaço)
+            Eaço = float(Eaço)
             Econc = float(Econc)
-            sigma_aco = float(sigma_aco)
-            sigma_conc = float(sigma_conc)
+            sigma_aço = eval(sigma_aço)
+            sigma_conc = eval(sigma_conc)
 
-            moment_of_resistance = calculate_moment_of_resistance(
-                b, d, Aaco, Eaco, Econc, sigma_aco, sigma_conc
+            x, Ieq, Mconc, Maço, Madm = calculate_moment_of_resistance(
+                b, d, Aaço, Eaço, Econc, sigma_aço, sigma_conc
             )
 
-            st.success(f"The moment of resistance is: {moment_of_resistance} N.m")
+            st.success(f"x: {x} m")
+            st.success(f"Ieq: {Ieq} m^4")
+            st.success(f"Mconc: {Mconc} N.m")
+            st.success(f"Maço: {Maço} N.m")
+            st.success(f"Madm: {Madm} N.m")
         except ValueError:
             st.error("Please enter valid numerical values")
 
